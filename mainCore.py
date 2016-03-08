c047abaab79bca588f6b05b6021b3558f917ea79
Zeumer <dezeumer@gmail.com>
2016-03-08 16:23:14 -0500

    New main file ready for testing

---
+++ mainCore.py
@@ -0,0 +1,38 @@
+# test BLE Scanning software
+# jcs 6/8/2014
+
+import blescan
+import sys
+import datetime
+import magFunctions
+
+import bluetooth._bluetooth as bluez
+
+dev_id = 0
+
+#file = open('logfile', 'w')
+
+try:
+	sock = bluez.hci_open_dev(dev_id)
+	print "ble thread started"
+
+except:
+	print "error accessing bluetooth device..."
+    	sys.exit(1)
+
+blescan.hci_le_set_scan_parameters(sock)
+blescan.hci_enable_le_scan(sock)
+
+magFunctions.init_magnetometer()
+
+while True:
+	returnedList = blescan.parse_events(sock, 1)
+	print "----------"
+#	print returnedList[0];
+	for beacon in returnedList:
+		print datetime.datetime.now()
+		print magFunctions.get_bearing()
+#		t = type(beacon)
+#		file.write(beacon)
+#		file.write('\n')		
+		print beacon

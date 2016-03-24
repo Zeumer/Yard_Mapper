b27f962cdd4551c7ce433d81cb7cc46a95b0b15f
Zeumer <dezeumer@gmail.com>
2016-03-24 18:26:38 -0400

    Testing to return beacon list

---
+++ bluetoothExample.py
@@ -0,0 +1,5 @@
+import bluetoothFunctions
+import packetFunctions
+
+
+createAllSockets()
--- packetFunctions.py
+++ packetFunctions.py
@@ -1,1 +1,54 @@
-#Goal of this class is to provide functions
+#Goal of this class is to provide functions for the manipulation of packets
+#e.g. reading UUID, Major, minor, transmit power
+import struct
+
+#readUUID
+def getUUID(stringPkt):
+	return stringPkt[0:31]
+#readMajor
+def readMajor(pkt):
+#	return unpackAsString(pkt[report_pkt_offset -6: report_pkt_offset - 4])
+
+#readMinor
+def readMinor(pkt):
+	#return unpackAsString(pkt[report_pkt_offset -4: report_pkt_offset - 2])
+
+#readMACAddress
+def readMACAddress(pkt):
+	return unpackAsString(pkt[report_pkt_offset + 3:report_pkt_offset + 9])
+
+#readTXPower
+def readMajor(pkt):
+#	return unpackAsString(pkt[report_pkt_offset + 3:report_pkt_offset + 9])
+
+#readRSSI
+
+#pktToString
+def pktToString(pkt):
+	report_pkt_offset = 0
+	Adstring = returnstringpacket(pkt[report_pkt_offset -22: report_pkt_offset - 6]) 
+    Adstring += ","
+    Adstring += "%i" % returnnumberpacket(pkt[report_pkt_offset -6: report_pkt_offset - 4]) 
+    Adstring += ","
+    Adstring += "%i" % returnnumberpacket(pkt[report_pkt_offset -4: report_pkt_offset - 2]) 
+    Adstring += ","
+    Adstring += "%i" % struct.unpack("b", pkt[report_pkt_offset -2])
+    Adstring += ","
+    Adstring += "%i" % struct.unpack("b", pkt[report_pkt_offset -1])
+
+    return Adstring
+
+#unpackAsString
+def returnstringpacket(pkt):
+    myString = "";
+    for c in pkt:
+        myString +=  "%02x" %struct.unpack("B",c)[0]
+    return myString 
+
+def returnnumberpacket(pkt):
+	myInteger = 0
+	multiple = 256
+	for c in pkt:
+	    myInteger +=  struct.unpack("B",c)[0] * multiple
+	    multiple = 1
+	return myInteger 
